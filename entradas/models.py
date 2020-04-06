49b056f3a19b7032bf53f8cfc982b70ddfac85b8
rex0747 <pedro.luis.jimenez@outlook.es>
2020-04-06 23:41:37 +0200

    29032020

--- entradas/models.py
+++ entradas/models.py
@@ -12,7 +12,7 @@ from agencias.models import agencias
 class entradas(models.Model):
     nAlbaran=models.CharField(max_length=8,null=False, verbose_name='Albaran')
     fk_proveedor=models.ForeignKey(proveedores,on_delete=models.CASCADE,blank=True)
-    fk_articulo=models.ForeignKey(articulos,on_delete=models.CASCADE,blank=True)
+    fk_articulo=models.CharField(max_length=8,blank=True) # models.ForeignKey(articulos,on_delete=models.CASCADE,blank=True)
     #nombre=models.CharField(max_length=70,null=True)
     fechaEntrada=models.DateTimeField(default=datetime.now, blank=True)
     unidades=models.IntegerField(blank=False)
@@ -20,7 +20,7 @@ class entradas(models.Model):
     peso=models.FloatField( default=0)
     agencia=models.ForeignKey(agencias,on_delete=models.CASCADE, blank=True,default='')
     precioPorte=models.FloatField(blank=True,default=True)
-    porte_pagado=models.NullBooleanField(default=True)
+    porte_pagado=models.BooleanField(default=True)
     #precioTotal= models.FloatField(default=0)
 
 
--- entradas/views.py
+++ entradas/views.py
@@ -3,15 +3,20 @@ from articulos.models import articulos
 from proveedores.models import proveedores
 from agencias.models import agencias
 from entradas.models import entradas
+from _datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
 def addEntrada(request):
-    nalbaran='';unidades='';referencia='';precunidad='';peso='';proveedor='';agencia='';precporte='';portepag='';
+    nalbaran='';unidades='';referencia='';precunidad='';peso='';proveedor='';
+    agencia='';precporte='';portepag='';error=False
 
     if request.method == 'POST':
         if request.POST['nalbaran']:
             nalbaran = request.POST['nalbaran']
             print(nalbaran)
+        else:
+            error=True
         if request.POST['ref']:
             referencia = request.POST['ref']
             print(referencia)
@@ -25,22 +30,29 @@ def addEntrada(request):
             peso = request.POST['peso']
             print(peso)
         if request.POST['proveedor']:
-            peso = request.POST['proveedor']
+            proveedor = request.POST['proveedor']
             print(proveedor)
         if request.POST['agencia']:
-            peso = request.POST['agencia']
+            agencia = request.POST['agencia']
             print(agencia)
         if request.POST['precporte']:
-            peso = request.POST['precporte']
+            precporte = request.POST['precporte']
             print(precporte)
         if request.POST['portepag']:
-            peso = request.POST['portepag']
+            portepag = request.POST['portepag']
             print(str(portepag))
 
-#    data=entradas(nAlbaran='',fk_proveedor='',fk_articulo='',FechaEntrada='',Unidades='',PrecioUnidad='',
-#                  Peso='',Agencia='',PrecioPorte='',porte_pagado='')
+    if error==True:
+        agencia = agencias.objects.all()
+        proveedor = proveedores.objects.all()
+        return render(request, 'addentrada.html',{'agencia': agencia, 'proveedor': proveedor})
 
+    print('Agencia: ' + str(agencia))
+    print('Proveedor: ' + str(proveedor))
+    fecha = datetime.now()
 
+    data=entradas(nAlbaran=nalbaran,fk_proveedor=int(proveedor),fk_articulo=referencia,FechaEntrada=fecha,
+    Unidades=unidades,PrecioUnidad=precunidad,Peso=peso,Agencia=int(agencia),PrecioPorte=precporte,porte_pagado=str(portepag))
 
     return render( request, 'addentrada.html')
 
