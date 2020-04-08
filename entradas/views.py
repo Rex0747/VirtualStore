9edaa10395891832119a0b768215277080c8086c
rex0747 <pedro.luis.jimenez@outlook.es>
2020-04-08 21:45:14 +0200

    29032020

--- entradas/views.py
+++ entradas/views.py
@@ -9,26 +9,28 @@ from django.http import HttpResponse
 # Create your views here.
 def addEntrada(request):
     nalbaran='';unidades='';referencia='';precunidad='';peso='';proveedor='';
-    agencia='';_agencia='';precporte='';portepag=False;error=False
+    agencia='';_agencia='';precporte='';portepag=False;error=False;_agencia='';_proveedor='';
 
     if request.method == 'POST':
         if request.POST['nalbaran']:
             nalbaran = request.POST['nalbaran']
             print(nalbaran)
-        else:
-            error=True
         if request.POST['ref']:
             referencia = request.POST['ref']
-            print(referencia)
+        else:
+            error=True
         if request.POST['unidades']:
             unidades = request.POST['unidades']
-            print(unidades)
+        else:
+            error=True
         if request.POST['precunid']:
             precunidad = request.POST['precunid']
-            print(precunidad)
+        else:
+            error=True
         if request.POST['peso']:
             peso = request.POST['peso']
-            print(peso)
+        else:
+            error=True
         if request.POST['proveedor']:
             proveedor = request.POST['proveedor']
             print(proveedor)
@@ -46,26 +48,37 @@ def addEntrada(request):
         else:
             portepag=False
 
-    if error==True:
+
+    if error==True or request.method != 'POST':
         agencia = agencias.objects.all()
         proveedor = proveedores.objects.all()
         return render(request, 'addentrada.html',{'agencia': agencia, 'proveedor': proveedor})
 
-    _agencia=agencias.objects.filter(id=agencia)
-    _proveedor=proveedores.objects.filter(id=proveedor)
+    try:
+        _agencia=agencias.objects.filter(id=agencia)
+        _proveedor=proveedores.objects.filter(id=proveedor)
+    except Exception as e:
+        print('Excepcion al canto: '+ str( e))
 
     print('Agencia: ' + str(_agencia))
     print('Proveedor: ' + str(proveedor))
     fecha = datetime.now()
     #from django.db import connection
     #print(connection.queries)
-    
+    if nalbaran == '':
+        nalbaran=entradas.objects.latest('id')
+        print('nalbaran: '+ str(nalbaran))
+        nalbaran = str( int( nalbaran.nAlbaran ) +  1 )
+        print('nalbaran: '+ nalbaran )
 
-    data=entradas(nAlbaran=nalbaran,fk_proveedor=_proveedor.get(id=proveedor) ,fk_articulo=referencia,fechaEntrada=fecha,
-    unidades=unidades,precioUnidad=precunidad,peso=peso,agencia=_agencia.get(id=agencia),precioPorte=precporte,porte_pagado=str(portepag))
-    data.save()
-
+    try:
+        data=entradas(nAlbaran=nalbaran,fk_proveedor=_proveedor.get(id=proveedor) ,fk_articulo=referencia,fechaEntrada=fecha,
+        unidades=unidades,precioUnidad=precunidad,peso=peso,agencia=_agencia.get(id=agencia),precioPorte=precporte,porte_pagado=str(portepag))
+        data.save()
+    except Exception as e:
+        print('ERROR: '+ str(e))
     agencia = agencias.objects.all()
     proveedor = proveedores.objects.all()
+
     return render( request, 'addentrada.html',{'agencia': agencia, 'proveedor': proveedor} )
 
